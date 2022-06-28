63fcdec52a60ebeb96af1d50641cae60dec4256b
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-28 23:41:05 +0800

    Create Test2.py

---
+++ Test2.py
@@ -0,0 +1,41 @@
+import sys
+
+
+def tambah(a, b):
+    hasil = a + b
+    return hasil
+
+def tolak(a, b):
+    hasil = b - a
+    return hasil
+
+def darab(a, b):
+    hasil = a * b
+    return hasil
+
+def bahagi(a, b):
+    hasil = a / b
+    return hasil
+
+def pilihan(tambah, tolak, darab, bahagi, pilih):
+    if (pilih == tambah):
+        return tambah(a, b)
+    elif (pilih == tolak):
+        return tolak(a, b)
+    elif (pilih == darab):
+        return darab(a, b)
+    elif (pilih == bahagi):
+        return bahagi(a, b)
+
+def get_input():
+    a = sys.argv[1]
+    b = sys.argv[2]
+    pilih = sys.argv [3]
+    return int(a), int(b), str(pilih)
+
+def main():
+    a, b, pilih = get_input()
+    hasil = pilihan(a,b,pilih)
+    print(hasil)
+
+main()
