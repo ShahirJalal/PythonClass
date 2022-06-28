516075e60e4e369ed63416f2d2d3584882ef4e29
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-29 00:20:46 +0800

    Update

--- Test3.py
+++ Test3.py
@@ -1,13 +1,11 @@
-from contextlib import nullcontext
 import sys
-
 
 def tambah(a, b):
     hasil = a + b
     return hasil
 
 def tolak(a, b):
-    hasil = b - a
+    hasil = a - b
     return hasil
 
 def darab(a, b):
@@ -18,29 +16,31 @@ def bahagi(a, b):
     hasil = a / b
     return hasil
 
-def pilihan(tambah, tolak, darab, bahagi, pilih):
-
-    a = 0
-    b = 0
-
-    if (pilih == "tambah"):
+def pilihan(a, b, pilih):
+    if pilih == "tambah":
         return tambah(a, b)
-    elif (pilih == "tolak"):
+    elif pilih == "tolak":
         return tolak(a, b)
-    elif (pilih == "darab"):
+    elif pilih == "darab":
         return darab(a, b)
-    elif (pilih == "bahagi"):
+    elif pilih == "bahagi":
         return bahagi(a, b)
+
 
 def get_input():
     a = sys.argv[1]
     b = sys.argv[2]
-    pilih = sys.argv [3]
+    pilih = sys.argv[3]
     return int(a), int(b), str(pilih)
 
 def main():
-    a, b, pilih = get_input()
-    hasil = pilihan(a,b,pilih)
-    print(hasil)
+
+    try:
+        a, b, pilih = get_input()
+        hasil = pilihan(a, b, pilih)
+        print(hasil)
+    
+    except Exception as e:
+        print("Tak jadi bro")
 
 main()
