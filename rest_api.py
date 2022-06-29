26a5e5c5f5c11c71962bb40200229e76b37a7985
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-29 11:24:10 +0800

    Update rest_api.py

--- rest_api.py
+++ rest_api.py
@@ -1,4 +1,5 @@
 from cgitb import reset
+from time import time
 from urllib import response
 import requests
 
@@ -7,4 +8,5 @@ waktu_solat_api = "https://waktu-solat-api.herokuapp.com/api/v1/states.json"
 response = requests.get(waktu_solat_api)
 data = response.json()
 
-print(data['data']['negeri'][0]['zon'][0]['waktu_solat'][0]['time'])
+for maklumat in (data['data']['negeri'][0]['zon'][0]['waktu_solat']):
+    print(time)
