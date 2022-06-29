ff5d10e207f31c7b789dc72dddd97b11f72759d1
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-29 17:52:43 +0800

    Update

---
+++ Resume.py
@@ -0,0 +1,62 @@
+{
+    "resume": [
+        {
+
+        }
+    ]
+}
+
+"""
+Personal Information:
+
+    Name: Muhammad Shahir Bin Jalal Hazli
+    Location: Kuala Lumpur, Malaysia
+    Phone_number: +60133879365
+    Email: shahirjalalhazli@gmail.com
+    Portfolio: github.com/ShahirJalal
+
+Educations:
+
+    Institution: Universiti Teknologi PETRONAS
+    Course: Bachelor of Science (Honours) Applied Physics
+    Relevant coursework: Electromagnetism, Quantum Mechanics, Optics,
+                         Vibration and Waves, Atomic Physics, Solid State
+                         Physics, Semiconductor and Devices Technology
+    Award: Silver Medal in Science & Engineering Design Exhibition 2018,
+           Gold Medal in Science & Engineering Design Exhibition 2019
+    Club: Fundamental of Applied Science Student Ambassador (FASSA) as
+          Human Resource, Media and Technology Club (MedTech) as committee
+
+    Institution: SMK Taman Melati
+    Award: Fourth Place in ASEAN Quiz 2014 and Gold Medal in Johor 
+    International Jamboree 2013
+
+Working Experiences:
+
+    Company: IOP Specialists Sdn. Bhd.
+    Duration: 2021 - 2022
+    Position: Production Engineer
+    Work: • Manage production processes and diagnose problems in the production line.
+          • Ensure the achievement of daily production targets.
+          • Work closely with R&D and QC department on final production output.
+          • Troubleshoot the root cause of production defects and problems by performing
+            a Corrective Action Report (CAR)
+
+    Company: PETRONAS Research Sdn. Bhd. (PRSB)
+    Duration: 2019 - 2019
+    Position: Intern
+    Work: • Assisted in sand monitoring in pipeline by using a fiber optic cable.
+          • Assisted in assembling the fiber optic sensor system to the pipeline.
+          • Learnt to use Optic Interrogator and interpret data produced from it.
+          • Done some market researches and literature reviews for the experiment.
+
+    Company: Universiti Teknologi PETRONAS (UTP)
+    Duration: 2017 - 2017
+    Position: Research Assistant
+    Work: • Prepared Dye-Sensitized Solar Cells (DSSCs) samples needed to perform 
+            experiments.
+          • Assisted in preparing Carbon Nanotubes needed for the research.
+          • Learnt how Electron Microscope works and how to interpret data produced.
+          • Done some literature reviews related to the research.
+
+"""
