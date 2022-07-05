45544dddca2c8e33f6ededb9fab8770c44f625ca
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-07-06 04:32:23 +0800

    Create LagiTest3.py

---
+++ LagiTest3.py
@@ -0,0 +1,14 @@
+mydict = {'fruits': ['banana', 'apple', 'orange'],
+         'vegetables': ['pepper', 'carrot'], 
+         'cheese': ['swiss', 'cheddar', 'brie']}
+
+# item = 'cheddar'
+# for key in mydict.keys():
+#     if item in mydict[key]:
+#         print(key)
+
+for key in mydict.keys():
+    if key == 'cheese':
+        for item in mydict['cheese']:
+            if item == 'swiss':
+                print(item)
